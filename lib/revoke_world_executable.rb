c5a1f2f0255438e8aaeb5aef06a1a5f5a73575cc
Bassel Samman <bassel.samman@hp.com>
2015-11-08 22:06:23 -0600

    Made test work

--- lib/revoke_world_executable.rb
+++ lib/revoke_world_executable.rb
@@ -3,6 +3,4 @@ require_relative 'revoke_world_executable/world_executable_fixer'
 require_relative 'revoke_world_executable/runner_options'
 require_relative 'revoke_world_executable/runner'
 
-module RevokeWorldExecutable
-  # Your code goes here...
-end
+module RevokeWorldExecutable ; end
